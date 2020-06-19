8c06bc5df1c3419b1423132809edcc915e282f9d
Carlos Fernandez <jcfernan@live.com>
2020-06-18 23:38:08 -0600

    Finish up plus and minus button

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,15 @@
 const counter = document.querySelector("#counter")
+const minus = document.querySelector("#minus")
 const plus = document.querySelector("#plus")
 
+minus.addEventListener("click", decrementCounter)
 plus.addEventListener("click", incrementCounter)
 
+function decrementCounter () {
+    const currentCount = parseInt(counter.textContent, 10);
+    counter.textContent = `${currentCount - 1}`;
+}
 function incrementCounter () {
     const currentCount = parseInt(counter.textContent, 10);
-    console.log(currentCount + 1);
+    counter.textContent = `${currentCount + 1}`;
 }
